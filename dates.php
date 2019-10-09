de010ec37396b2bfa1d5d716f34e248c8ca3ad92
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-08 23:31:34 -0500

    Update dates.php

--- dates.php
+++ dates.php
@@ -11,3 +11,15 @@ function getFirstDayOfDate($date)
     $d->modify('first day of this month');
     return $d->format('Y-m-d');
 }
+
+/**
+ * Devuelve la fecha del ultimo dia del mes de una fecha especifica
+ * @param string $date Fecha. Ejem: 2018-03-14
+ * @return string Ejem: 2018-03-31
+ */
+function getLastDayOfDate($date)
+{
+    $d = new DateTime($date);
+    $d->modify('last day of this month');
+    return $d->format('Y-m-d');
+}
