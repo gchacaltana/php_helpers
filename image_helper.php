7362133dd2ef4a8831b0c676fb9e2c9a1ad307d6
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2022-08-28 01:32:13 -0500

    update image helper - add function getImageDataUri

--- image_helper.php
+++ image_helper.php
@@ -1,7 +1,10 @@
 <?php
 
+declare( strict_types=1 );
+
 /**
- * Resize image - Generar imagen con nuevo tamaño.
+ * Función para generar imagen con nuevo tamaño.
+ * 
  * @param string $sourceImage Ruta de ubicación de la imagen JPEG
  * @param string $targetImage ruta destino de la imagen JPEG
  * @param int $maxWidth Ancho máximo de la imagen final (value 0 - $maxWidth is optional)
@@ -9,9 +12,8 @@
  * @param int $quality Calidad de la imagen final (0-100)
  * @return bool
  */
-function resizeImage($sourceImage, $targetImage, $maxWidth, $maxHeight,
-                     $quality = 80)
-{
+function resizeImage(string $sourceImage, string $targetImage, int $maxWidth, int $maxHeight,
+        int $quality = 80): bool {
     if (!$image = @imagecreatefromjpeg($sourceImage)) {
         return FALSE;
     }
@@ -28,21 +30,30 @@ function resizeImage($sourceImage, $targetImage, $maxWidth, $maxHeight,
     }
 
     // Calculando la proporción de tamaños máximos
-    $widthRatio  = $maxWidth / $origWidth;
+    $widthRatio = $maxWidth / $origWidth;
     $heightRatio = $maxHeight / $origHeight;
 
     // Calculando nuevas dimensiones de la imagen
-    $ratio     = min($widthRatio, $heightRatio);
-    $newWidth  = (int) $origWidth * $ratio;
+    $ratio = min($widthRatio, $heightRatio);
+    $newWidth = (int) $origWidth * $ratio;
     $newHeight = (int) $origHeight * $ratio;
 
     // Creando imagen final con nuevas dimensiones
     $newImage = imagecreatetruecolor($newWidth, $newHeight);
     imagecopyresampled($newImage, $image, 0, 0, 0, 0, $newWidth, $newHeight,
-        $origWidth, $origHeight);
+            $origWidth, $origHeight);
     imagejpeg($newImage, $targetImage, $quality);
     // Liberar memoria.
     imagedestroy($image);
     imagedestroy($newImage);
     return TRUE;
+}
+
+/**
+ * Función que retorna el data uri de una imagen
+ * @param string $image Ruta y nombre de imagen
+ * @param string $mime Tipo MIME de la imagen
+ */
+function getDataURI(string $image, string $mime = ""): string {
+    return 'data: ' . (function_exists('mime_content_type') ? mime_content_type($image) : $mime) . ';base64,' . base64_encode(file_get_contents($image));
 }
